431d8a61b414dfe1dcfedcefc4fd7ab93784fa96
Michael John Peña <mjtpena@outlook.com>
2018-12-06 06:45:50 +1100

    Abstracted the subscriptionkey outside the service class

--- test/language/translate_text_test.dart
+++ test/language/translate_text_test.dart
@@ -4,9 +4,10 @@ import 'package:cognitive_services/language/translate_text_service.dart';
 
 void main() {
   test('It translates from English to Spanish', () async {
+    String subsKey = "863c1c7b0293450396d16ea6d8794dbb";
     final translator = TranslateTextService();
     final message = TranslateTextRequest(text: "Hello World!").toJson();
-    var response = await translator.fetch(message);
+    var response = await translator.fetch(message, subsKey);
     expect(response.translations, isNotNull);
   });
 }
